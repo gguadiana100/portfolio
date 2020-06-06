81942a36005a477df45a0d634ce8a25dedcfb4e6
gguadiana100 <61398921+gguadiana100@users.noreply.github.com>
2020-06-05 21:17:34 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,26 @@
+
+  document.getElementById("navigation").innerHTML =
+  `<a href="landing.html">
+    <h1>Gilberto Guadiana</h1>
+  </a>
+  <a href="landing.html">
+    <img src="images/mugshot.jpg">
+  </a>
+
+  <div class="nav-options">
+      <a href="landing.html">
+        <li> Home </li>
+      </a>
+      <a href="projects.html">
+        <li> Projects </li>
+      </a>
+      <a href="https://www.facebook.com/nublockchain/">
+        <li> Blockchain </li>
+      </a>
+      <a href="Resume.pdf">
+        <li> Resume </li>
+      </a>
+      <a href="bio.html">
+        <li> Bio </li>
+      </a>
+  </div>`;
